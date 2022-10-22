4e37cdbc449f49b672580641cfe5b8b05204faf1
Yuxue Yang <534533683@qq.com>
2022-10-23 00:12:06 +0800

    add scatter_sum without test

---
+++ torchex/src/utils/timer.cuh
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdio>
+
+struct GPUTimer {
+    cudaEvent_t beg, end;
+    GPUTimer() {
+        cudaEventCreate(&beg);
+        cudaEventCreate(&end);
+    }
+    ~GPUTimer() {
+        cudaEventDestroy(beg);
+        cudaEventDestroy(end);
+    }
+    void start() {
+        cudaEventSynchronize(beg);
+        cudaEventRecord(beg, 0);
+    }
+    double stop() {
+        cudaEventRecord(end, 0);
+        cudaEventSynchronize(end);
+        float ms;
+        cudaEventElapsedTime(&ms, beg, end);
+        printf("time of cudaMalloc and Memset %.4f ms\n", ms);
+        return ms;
+    }
+};
