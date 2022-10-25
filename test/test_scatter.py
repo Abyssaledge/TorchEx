6b3cd77af9577af0df29ffd3a1a3071585092eb9
YuxueYang <534533683@qq.com>
2022-10-25 23:50:29 +0800

    fix and wrap scatter_sum

--- test/test_scatter.py
+++ test/test_scatter.py
@@ -1,26 +1,37 @@
 import torch
 import numpy as np
 import torch_scatter
-from torchex import scatter_sum
+from torchex import scatter_sum, TorchTimer
 import time
+freq = 5
+timer_torch_scatter = TorchTimer(freq)
+timer_torchex = TorchTimer(freq)
 np.random.seed(0)
-device = torch.device("cuda:8")
-unq_inv = torch.tensor([], dtype=torch.int32, device=device)
-unq_preSum = torch.zeros((501,1), dtype=torch.int32, device=device)
+device = torch.device("cuda:0")
+channel = 128
+num_group = 500
+unq_inv = torch.tensor([], dtype=int, device=device)
 cumsum = 0
-for group_id in range(500):
+for group_id in range(num_group):
     cnt = np.random.randint(100, 1000)
-    cumsum += cnt
-    unq_preSum[group_id+1] = cumsum
     unq_inv = torch.cat((unq_inv, unq_inv.new_ones(cnt)*group_id), dim=0)
 unq_inv.unsqueeze_(-1)
-assert unq_preSum[-1,0] == unq_inv.shape[0]
-feat = unq_inv.new_ones((unq_inv.shape[0],128), dtype=torch.float32)
+feat = unq_inv.new_ones((unq_inv.shape[0], channel), dtype=torch.float32)
 # start = time.time()
-# ans = torch_scatter.scatter(feat, unq_inv, dim=0, reduce='sum')
+# ans1 = torch_scatter.scatter(feat, unq_inv, dim=0, reduce='sum')
 # torch.cuda.synchronize()
-# print(f'time cost of torch_scatter:{(time.time()-start)*1000:.4f}ms')
-start = time.time()
-ans = scatter_sum(feat, unq_inv)
-torch.cuda.synchronize()
-print(f'time cost of torch_scatter:{(time.time()-start)*1000:.4f}ms')
+# print(f'time cost of torch_scatter:\t\t{(time.time()-start)*1000:2.4f}ms')
+# start = time.time()
+# ans2 = scatter_sum(feat, unq_inv)
+# torch.cuda.synchronize()
+# print(f'time cost of scatter_sum(Torchex):\t{(time.time()-start)*1000:2.4f}ms')
+# print(f'check:\t{torch.all((ans1-ans2) < 1e-3)}')
+
+for i in range(100):
+    with timer_torch_scatter.timing('torch_scatter'):
+        ans1 = torch_scatter.scatter(feat, unq_inv, dim=0, reduce='sum')
+    with timer_torchex.timing('Torchex'):
+        ans2 = scatter_sum(feat, unq_inv)
+    if (i+1)%freq == 0:
+        flag = bool(((ans1-ans2)<1e-2).all().cpu())
+        print(f'check:\t{flag}')
--- torchex/operator_py/scatter_op.py
+++ torchex/operator_py/scatter_op.py
@@ -3,6 +3,7 @@ from torch import nn as nn
 from torch.autograd import Function
 import scatter_ext
 
+
 class ScatterSumFunction(Function):
 
     @staticmethod
@@ -10,10 +11,11 @@ class ScatterSumFunction(Function):
         """connected_components function forward.
         Args:
             src (torch.Tensor): [num_total, channel]
-            index (torch.Tensor): [num_total]
+            index (torch.Tensor, dtype:int32): [num_total]
             num_unq (int): the number of unique sets
         """
         num_unq = index[-1] + 1
+        index = index.int()
         out = src.new_zeros((num_unq, src.shape[1]))
         scatter_ext.sum(src, index, out)
 
@@ -25,5 +27,6 @@ class ScatterSumFunction(Function):
     def backward(ctx, g):
         # TODO
         return None, None, None
+
 
 scatter_sum = ScatterSumFunction.apply
