59da50fde7f738ba744cef35873ff2cf374e510a
Yuxue Yang <534533683@qq.com>
2022-10-25 18:04:02 +0800

    add scatter_sum for test

--- setup.py
+++ setup.py
@@ -38,6 +38,31 @@ setup(
         #      './torchex/src/group_fps/group_fps_kernel.cu',]
         # ),
         CUDAExtension(
+            'weighted_nms_ext', 
+            ['./torchex/src/weighted_nms/wnms.cpp',
+             './torchex/src/weighted_nms/wnms_kernel.cu',]
+        ),
+        CUDAExtension(
+            'dynamic_point_pool_ext', 
+            ['./torchex/src/dynamic_point_pool/dynamic_point_pool.cpp',
+             './torchex/src/dynamic_point_pool/dynamic_point_pool_kernel.cu',]
+        ),
+        CUDAExtension(
+            'sparse_roi_voxelization', 
+            ['./torchex/src/sparse_roi_voxelization/sparse_roiaware_pool3d.cpp',
+             './torchex/src/sparse_roi_voxelization/sparse_roiaware_pool3d_kernel.cu',]
+        ),
+        CUDAExtension(
+            'ingroup_indices', 
+            ['./torchex/src/ingroup_inds/ingroup_inds.cpp',
+             './torchex/src/ingroup_inds/ingroup_inds_kernel.cu',]
+        ),
+        CUDAExtension(
+            'connected_components_labeling',
+            ['./torchex/src/connected_components/ccl.cpp',
+             './torchex/src/connected_components/ccl_kernel.cu',]
+        ),
+        CUDAExtension(
             'scatter_ext',
             ['./torchex/src/scatter/scatter.cpp',
              './torchex/src/scatter/scatter_kernel.cu']
