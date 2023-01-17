c1477851665e718c5d7791becc4a0494155d9927
YuxueYang <534533683@qq.com>
2023-01-17 18:16:06 +0800

    Add dynamic_point_pool_mixed_launcher

--- torchex/src/dynamic_point_pool/dynamic_point_pool_kernel.cu
+++ torchex/src/dynamic_point_pool/dynamic_point_pool_kernel.cu
@@ -170,6 +170,113 @@ __global__ void generate_pts_mask_for_box3d(
   }
 }
 
+__global__ void generate_pts_mask_for_box3d_mixed(
+  const int boxes_num, const int pts_num, const int max_pts_num, const int max_all_pts_num,
+  const float *rois,
+  const int *rois_batch,
+  const float *pts,
+  const int *pts_batch,
+  int *pts_mask,
+  float *pts_mask_info,
+  int *in_box_counter,
+  int *global_counter,
+  const int info_dim,
+  const float extra_w,
+  const float extra_l,
+  const float extra_h
+) {
+
+  // params rois: (N, 7) [x, y, z, w, l, h, rz] in LiDAR coordinate
+  // params rois_batch: (N, ) the batch index of each roi
+  // params pts: (npoints, 3) [x, y, z]
+  // params pts_batch: (npoints, ) the batch index of each point
+  // counter: [num_box, ]
+  // pts_mask [num_box, max_pts_num] 
+  // pts_mask_info [num_box, max_pts_num, info_dim] 
+
+  int pt_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int box_idx = blockIdx.y;
+  assert (box_idx < boxes_num);
+  if (pt_idx >= pts_num) return;
+
+  pts += pt_idx * 3;
+  rois += box_idx * 7;
+
+  float local_x = 0, local_y = 0;
+  int cur_in_flag = 0;
+  if (pts_batch[pt_idx] == rois_batch[box_idx]){
+    cur_in_flag = check_pt_in_box3d(pts, rois, local_x, local_y, extra_w, extra_l, extra_h);
+  }
+
+  if (cur_in_flag > 0) {
+
+    float w = rois[3], l = rois[4], h = rois[5];
+    float local_z = pts[2] - (rois[2] + h / 2.0); // to roi center
+
+    int cnt = atomicAdd(&in_box_counter[box_idx], 1);
+    if (cnt >= max_pts_num) return;
+
+    int new_pt_idx = atomicAdd(&global_counter[0], 1);
+    if (new_pt_idx >= max_all_pts_num) return;
+
+    float off_x = local_x + l / 2;
+    float off_y = local_y + w / 2;
+    float off_z = local_z + h / 2;
+
+    float off_x_2 = -local_x + l / 2;
+    float off_y_2 = -local_y + w / 2;
+    float off_z_2 = -local_z + h / 2;
+
+    float is_in_margin = cur_in_flag == 1 ? 1 : 0;
+
+    pts_mask[box_idx * max_pts_num * 2 + cnt * 2 + 0] = pt_idx;
+    pts_mask[box_idx * max_pts_num * 2 + cnt * 2 + 1] = new_pt_idx;
+
+    assert (info_dim == 13);
+    assert (boxes_num * max_pts_num * info_dim > box_idx * max_pts_num * info_dim + cnt * info_dim + 12);
+
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 0] = pts[0];
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 1] = pts[1];
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 2] = pts[2];
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 3] = local_x;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 4] = local_y;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 5] = local_z;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 6] = off_x;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 7] = off_y;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 8] = off_z;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 9] = off_x_2;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 10] = off_y_2;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 11] = off_z_2;
+    pts_mask_info[box_idx * max_pts_num * info_dim + cnt * info_dim + 12] = is_in_margin;
+
+
+    #ifdef ASSERTION
+    if (is_in_margin == 1){
+      assert (
+        (local_x <= -l/2 + 1e-4) | (local_x >= l/2 - 1e-4) | 
+        (local_y <= -w/2 + 1e-4) | (local_y >= w/2 - 1e-4) |
+        (local_z <= -h/2 + 1e-4) | (local_z >= h/2 - 1e-4)
+      );
+      assert (
+        ((off_x <= 0 + 1e-4) && (off_x >= -extra_l/2-1e-4)) || ((off_x >= l - 1e-4) && (off_x <= l + extra_l/2 + 1e-4)) ||
+        ((off_y <= 0 + 1e-4) && (off_y >= -extra_w/2-1e-4)) || ((off_y >= w - 1e-4) && (off_y <= w + extra_w/2 + 1e-4)) ||
+        ((off_z <= 0 + 1e-4) && (off_z >= -extra_h/2-1e-4)) || ((off_z >= h - 1e-4) && (off_z <= h + extra_h/2 + 1e-4))
+      );
+      assert (
+        ((off_x_2 <= 0 + 1e-4) && (off_x_2 >= -extra_l/2-1e-4)) || ((off_x_2 >= l - 1e-4) && (off_x_2 <= l + extra_l/2 + 1e-4)) ||
+        ((off_y_2 <= 0 + 1e-4) && (off_y_2 >= -extra_w/2-1e-4)) || ((off_y_2 >= w - 1e-4) && (off_y_2 <= w + extra_w/2 + 1e-4)) ||
+        ((off_z_2 <= 0 + 1e-4) && (off_z_2 >= -extra_h/2-1e-4)) || ((off_z_2 >= h - 1e-4) && (off_z_2 <= h + extra_h/2 + 1e-4))
+      );
+    }
+    if (is_in_margin == 2){
+      assert(off_x > 0 - 1e-5 && off_x_2 > 0 - 1e-5 && off_y > 0 - 1e-5 && off_y_2 > 0 - 1e-5 && off_z > 0 - 1e-5 && off_z_2 > 0 - 1e-5);
+      assert(off_x < l + 1e-5 && off_x_2 < l + 1e-5 && off_y < w + 1e-5 && off_y_2 < w + 1e-5 && off_z < h + 1e-5 && off_z_2 < h + 1e-5);
+    }
+    #endif
+
+  }
+}
+
 __global__ void collect_inside_points(
   const int boxes_num,
   const int pts_num,
@@ -256,6 +363,104 @@ __global__ void collect_inside_points(
     boxes_num, pts_num, max_num_pts_per_box, max_all_pts_num,
     rois,
     pts,
+    pts_mask,
+    pts_mask_info,
+    inbox_counter,
+    global_counter,
+    info_dim,
+    extra_w,
+    extra_l,
+    extra_h
+  );
+  #ifdef DEBUG
+  printf("Finish mask generation.\n");
+  CHECK_CALL(cudaGetLastError());
+  CHECK_CALL(cudaDeviceSynchronize());
+  #endif
+
+  int new_pts_num[1];
+  CHECK_CALL(cudaMemcpy(new_pts_num, global_counter, sizeof(int), cudaMemcpyDeviceToHost));
+
+  dim3 block_collect(DIVUP(max_num_pts_per_box, THREADS_PER_BLOCK), boxes_num);
+  dim3 thread_collect(THREADS_PER_BLOCK);
+
+
+  collect_inside_points<<<block_collect, thread_collect>>>(
+    boxes_num,
+    pts_num,
+    new_pts_num[0],
+    max_num_pts_per_box,
+    info_dim,
+    pts_mask,
+    pts_mask_info,
+    out_pts_idx, out_roi_idx, out_pts_feats
+  );
+
+
+  cudaFree(inbox_counter);
+  cudaFree(global_counter);
+  cudaFree(pts_mask);
+  cudaFree(pts_mask_info);
+
+  #ifdef DEBUG
+  CHECK_CALL(cudaGetLastError());
+  CHECK_CALL(cudaDeviceSynchronize());
+  #endif
+
+  return;
+
+}
+
+void dynamic_point_pool_mixed_launcher(
+  int boxes_num,
+  int pts_num, 
+  int max_num_pts_per_box,
+  int max_all_pts_num,
+  const float *rois,
+  const int *rois_batch,
+  const float *pts,
+  const int *pts_batch,
+  const float *extra_wlh,
+  long *out_pts_idx,
+  long *out_roi_idx,
+  float *out_pts_feats,
+  int info_dim
+  ) {
+  
+  #ifdef DEBUG
+  printf("pts_num: %d, boxes_num: %d, max_num: %d \n", pts_num, boxes_num, max_num_pts_per_box);
+  #endif
+
+  int *inbox_counter = NULL;
+  CHECK_CALL(cudaMalloc(&inbox_counter,   boxes_num * sizeof(int)));
+  CHECK_CALL(cudaMemset(inbox_counter, 0, boxes_num * sizeof(int)));
+
+  int *global_counter = NULL;
+  CHECK_CALL(cudaMalloc(&global_counter,   1 * sizeof(int)));
+  CHECK_CALL(cudaMemset(global_counter, 0, 1 * sizeof(int)));
+
+  int *pts_mask = NULL;
+  CHECK_CALL(cudaMalloc(&pts_mask,    boxes_num * max_num_pts_per_box * 2 * sizeof(int)));
+  CHECK_CALL(cudaMemset(pts_mask, -1, boxes_num * max_num_pts_per_box * 2 * sizeof(int)));
+
+  float *pts_mask_info = NULL;
+  CHECK_CALL(cudaMalloc(&pts_mask_info,    boxes_num * max_num_pts_per_box * info_dim * sizeof(float)));
+  CHECK_CALL(cudaMemset(pts_mask_info, 0,  boxes_num * max_num_pts_per_box * info_dim * sizeof(float)));
+
+
+
+  float extra_w = extra_wlh[0];
+  float extra_l = extra_wlh[1];
+  float extra_h = extra_wlh[2];
+  dim3 blocks_mask(DIVUP(pts_num, THREADS_PER_BLOCK), boxes_num);
+  dim3 threads(THREADS_PER_BLOCK);
+
+  generate_pts_mask_for_box3d_mixed<<<blocks_mask, threads>>>(
+    boxes_num, pts_num, max_num_pts_per_box, max_all_pts_num,
+    rois,
+    rois_batch,
+    pts,
+    pts_batch,
     pts_mask,
     pts_mask_info,
     inbox_counter,
